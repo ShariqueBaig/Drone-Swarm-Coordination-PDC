d13d9847753fef6e5d2402bd51d084c527efffa3
Sharique Baig <sharique.baig@iba.edu.pk>
2026-02-20 17:32:27 +0500

    added 3d simulation

--- boids/config.py
+++ boids/config.py
@@ -14,6 +14,7 @@ separation_weight = 1.5
 alignment_weight = 1.0
 cohesion_weight = 1.0
 obstacle_weight = 5.0  # Weight for avoiding obstacles
+boundary_weight = 1.0  # Weight for staying within bounds
 
 # Colors
 BACKGROUND_COLOR = (30, 30, 30)
@@ -22,3 +23,4 @@ OBSTACLE_COLOR = (255, 69, 0)  # Red-Orange
 
 # Obstacle parameters
 obstacle_radius = 40
+boundary_margin = 100 # Distance from wall to start steering away
